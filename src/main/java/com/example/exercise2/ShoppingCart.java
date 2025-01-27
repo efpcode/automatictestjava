0a4c04a55e35a0b87b4d5f7b293bf59db0e406f7
efpcode <efpjobhunt@gmail.com>
2025-01-27 18:49:47 +0100

    Refactors add method to addItem
    Adds deleteItem method testcase condition
    
    Adds getItemPrice method with condition

--- src/main/java/com/example/exercise2/ShoppingCart.java
+++ src/main/java/com/example/exercise2/ShoppingCart.java
@@ -1,10 +1,25 @@
 package com.example.exercise2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ShoppingCart {
-    List<String> items;
+    List<String> items = new ArrayList<>();
 
-    public void add(String itemName) {
+    public void addItem(String itemName) {
+        items.add(itemName);
+    }
+
+    public void deleteItem(String itemName) {
+        items.remove(itemName);
+    }
+
+    public int size() {
+        return items.size();
+
+    }
+
+    public double getItemPrice(String apple) {
+        return 0.0;
     }
 }
--- src/test/java/com/example/exercise2/ShoppingCartTest.java
+++ src/test/java/com/example/exercise2/ShoppingCartTest.java
@@ -15,10 +15,36 @@ public class ShoppingCartTest {
     ShoppingCart shoppingCart = new ShoppingCart();
 
     @Test
-    @DisplayName("Adding Item to shopping cart")
-    void addingItemToShoppingCart() {
-        shoppingCart.add("apple");
+    @DisplayName("Adding Item to shopping cart test")
+    void addingItemToShoppingCartTest() {
+        shoppingCart.addItem("apple");
         assertThatList(shoppingCart.items).contains("apple");
+
+    }
+
+    @Test
+    @DisplayName("deleteItemInShoppingCartTest")
+    void deleteItemInShoppingCartTest() {
+        shoppingCart.addItem("apple");
+        shoppingCart.addItem("pineapple");
+        shoppingCart.deleteItem("apple");
+        assertThatList(shoppingCart.items).containsExactly("pineapple");
+        assertThatList(shoppingCart.items).size().isEqualTo(1);
+
+    }
+
+    @Test
+    @DisplayName("Shopping Cart Items is of Length zero at Start Test")
+    void shoppingCartItemsIsOfLengthZeroAtStartTest() {
+        assertThat(shoppingCart.size()).isEqualTo(0);
+
+    }
+
+    @Test
+    @DisplayName("getItemPriceTest")
+    void getItemPriceTest() {
+        shoppingCart.addItem("apple");
+        assertThat(shoppingCart.getItemPrice("apple")).isEqualTo(5.00);
 
     }
 
