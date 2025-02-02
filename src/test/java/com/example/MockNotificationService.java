d73b19ea4cdbf057206c193e61adc75b5b81345e
efpcode <efpjobhunt@gmail.com>
2025-02-02 12:56:25 +0100

    Adds Mock for notification service

---
+++ src/test/java/com/example/MockNotificationService.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class MockNotificationService implements NotificationService {
+    @Override
+    public void sendBookingConfirmation(Booking booking) throws NotificationException {
+
+    }
+
+    @Override
+    public void sendCancellationConfirmation(Booking booking) throws NotificationException {
+
+    }
+}
